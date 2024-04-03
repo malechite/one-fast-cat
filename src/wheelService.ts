cb253915f3803d562639ca388156ffce8a0b2755
Brandon Everhart <b@everhart.io>
2024-04-02 20:36:05 -0500

    changes

--- src/wheelService.ts
+++ src/wheelService.ts
@@ -15,7 +15,6 @@ let lastTickTime = 0;
 let status = Status.Idle;
 let currentSpeed = 0; // in miles per hour
 let activityCheckInterval: NodeJS.Timeout | null = null;
-let speedUpdateInterval: NodeJS.Timeout | null = null;
 
 const updateStatus = () => {
   if (status === Status.Idle && tickCount > HALF_ROTATION) {
@@ -37,22 +36,10 @@ const resetIdleInterval = () => {
   }, 10000); // 10 seconds of inactivity
 };
 
-const resetSpeedInterval = () => {
-  if (speedUpdateInterval) clearInterval(speedUpdateInterval);
-  speedUpdateInterval = setInterval(() => {
-    if (status === Status.Active) {
-      console.log(`Current Speed: ${currentSpeed.toFixed(2)} mph`);
-    }
-  }, 500);
-};
-
 const calculateSpeed = () => {
   if (tickCount === 0) {
-    // Calculate speed only at the end of a rotation
     const currentTime = Date.now();
     const elapsedTimeInSeconds = (currentTime - lastTickTime) / 1000; // Convert ms to seconds
-    const distanceCovered = CIRCUMFERENCE_INCHES / INCHES_PER_MILE; // Distance covered per rotation in miles
-    currentSpeed = (distanceCovered / elapsedTimeInSeconds) * SECONDS_PER_HOUR; // Speed in mph
 
     console.log(
       `Elapsed Time for one rotation: ${elapsedTimeInSeconds} seconds`
@@ -71,9 +58,8 @@ const handleTick = (tick: number) => {
     rotationCount++;
     tickCount = 0;
     onRotationComplete();
-    calculateSpeed();
-    lastTickTime = currentTime; // Update at the start of a new rotation
   }
+  lastTickTime = currentTime;
 
   console.log(
     `Tick Count: ${tickCount}, State: ${status}, Current Speed: ${currentSpeed.toFixed(
@@ -85,14 +71,11 @@ const handleTick = (tick: number) => {
 const init = () => {
   lastTickTime = Date.now();
   if (!activityCheckInterval) resetIdleInterval();
-  if (!speedUpdateInterval) resetSpeedInterval();
 };
 
 const kill = () => {
   if (activityCheckInterval) clearTimeout(activityCheckInterval);
-  if (speedUpdateInterval) clearInterval(speedUpdateInterval);
   activityCheckInterval = null;
-  speedUpdateInterval = null;
   console.log(`WheelService stopped.`);
 };
 
